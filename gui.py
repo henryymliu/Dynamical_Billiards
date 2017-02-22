de526b727b37ee3bbc73b5203e9ab40d7b5269f7
Henry Liu <henryymliu@gmail.com>
2017-02-21 22:09:34 -0800

    fixed some spacing to satisfy PEP, moved stadium and ball formation tuples to top of initialise() for easier access

--- gui.py
+++ gui.py
@@ -4,66 +4,76 @@ import simulation as sim
 from PIL import Image, ImageTk
 import platform
 
+
 class simulation(tkinter.Tk):
-    def __init__(self,parent):
-        tkinter.Tk.__init__(self,parent)
+    def __init__(self, parent):
+        tkinter.Tk.__init__(self, parent)
         self.parent = parent
         self.initialize()
 
     def initialize(self):
-        self.grid() #sets up grid
+        # Stadium selection
+        stadiums = ('Test1', 'Test2')
+        # Ball Formation Selection
+        ballFormations = ('Test1', 'Test2')
+
+        self.grid()  # sets up grid
 
         # define a button to start simulation, runs startSimulation() when
         # clicked
-        button = tkinter.Button(self,text=u'Start simulation',
-            command=self.startSimulation)
-        button.grid(column=1,row=9)
-        #open image using PIL
+        button = tkinter.Button(self, text=u'Start simulation',
+                                command=self.startSimulation)
+        button.grid(column=1, row=9)
+        # open image using PIL
         if platform.system == 'Windows':
-            image=Image.open('images\HLS-EFS-CSC-Owl.bmp')
+            image = Image.open('images\HLS-EFS-CSC-Owl.bmp')
         else:
-            image=Image.open('images/HLS-EFS-CSC-Owl.bmp')
-        #make Tk compatible PhotoImage object, must save as object parameter
-        #to avoid garbage collection
-        self.photo=ImageTk.PhotoImage(image)
-        #make canvas and add photo
-        preview = tkinter.Canvas(self,width=275, height=183)
-        preview.grid(column=2,row=1)
-        preview.create_image(0,0, anchor='nw',image=self.photo)
+            image = Image.open('images/HLS-EFS-CSC-Owl.bmp')
+        # make Tk compatible PhotoImage object, must save as object parameter
+        # to avoid garbage collection
+        self.photo = ImageTk.PhotoImage(image)
+        # make canvas and add photo
+        preview = tkinter.Canvas(self, width=275, height=183)
+        preview.grid(column=2, row=1)
+        preview.create_image(0, 0, anchor='nw', image=self.photo)
 
+        self.grid_columnconfigure(0, weight=1)
+        # self.resizable(True,False)
 
+        self.stadiumList = Pmw.ComboBox(self, label_text='Choose Stadium', labelpos='nw', selectioncommand=None,
+                                        scrolledlist_items=stadiums, dropdown=1)
+        self.stadiumList.grid(column=0, row=1)
+        self.stadiumList.selectitem(0)
 
-        self.grid_columnconfigure(0,weight=1)
-        #self.resizable(True,False)
+        self.ballFormationList = Pmw.ComboBox(self, label_text='Choose Ball Formation', labelpos='nw',
+                                              selectioncommand=None,
+                                              scrolledlist_items=ballFormations, dropdown=1)
+        self.ballFormationList.grid(column=0, row=2)
+        self.ballFormationList.selectitem(0)
 
-        self.stadiumList = Pmw.ComboBox(self, label_text = 'Choose Stadium', labelpos = 'nw', selectioncommand = None,
-                                   scrolledlist_items = ('Test1', 'Test2'), dropdown=1)
-        self.stadiumList.grid(column = 0, row = 1);
-
-        self.ballFormationList = Pmw.ComboBox(self, label_text='Choose Ball Formation', labelpos='nw', selectioncommand=None,
-                                   scrolledlist_items=('Test1', 'Test2'), dropdown=1)
-        self.ballFormationList.grid(column=0, row=2);
-
-        self.initialVelScale = tkinter.Scale(self, from_=-10, to=10, orient=tkinter.HORIZONTAL, label='Initial Velocity')
-        self.initialVelScale.grid(column = 0, row = 3,columnspan=2, sticky='W'+'E');
+        self.initialVelScale = tkinter.Scale(self, from_=-10, to=10, orient=tkinter.HORIZONTAL,
+                                             label='Initial Velocity')
+        self.initialVelScale.grid(column=0, row=3, columnspan=2, sticky='W' + 'E')
 
         # note to bound these next two sliders based on size of stadium
-        self.initialXScale = tkinter.Scale(self, from_=-10, to=10 , orient=tkinter.HORIZONTAL, label='Initial Position (X)')
-        self.initialXScale.grid(column=0, row=4,columnspan=2, sticky='W'+'E');
+        self.initialXScale = tkinter.Scale(self, from_=-10, to=10, orient=tkinter.HORIZONTAL,
+                                           label='Initial Position (X)')
+        self.initialXScale.grid(column=0, row=4, columnspan=2, sticky='W' + 'E')
 
-        self.initialYScale = tkinter.Scale(self, from_=-10, to=10, orient=tkinter.HORIZONTAL, label='Initial Position (Y)')
-        self.initialYScale.grid(column=0, row=5,columnspan=2, sticky='W'+'E');
+        self.initialYScale = tkinter.Scale(self, from_=-10, to=10, orient=tkinter.HORIZONTAL,
+                                           label='Initial Position (Y)')
+        self.initialYScale.grid(column=0, row=5, columnspan=2, sticky='W' + 'E')
 
-        self.playbackSpeedScale = tkinter.Scale(self, from_=-10, to=10, orient=tkinter.HORIZONTAL, label='Playback Speed')
-        self.playbackSpeedScale.grid(column=0, row=8 ,columnspan=2, sticky='W'+'E');
+        self.playbackSpeedScale = tkinter.Scale(self, from_=-10, to=10, orient=tkinter.HORIZONTAL,
+                                                label='Playback Speed')
+        self.playbackSpeedScale.grid(column=0, row=8, columnspan=2, sticky='W' + 'E')
 
         self.toTrace = False
         traceCheck = tkinter.Checkbutton(self, text="Trace", variable=self.toTrace)
         traceCheck.grid(column=2, row=9, sticky='W')
 
     def startSimulation(self):
-        # TODO: Handle unselected combobox case
-        simArgs=dict()
+        simArgs = dict()
         simArgs['stadium'] = self.stadiumList.get(first=None, last=None)
         simArgs['ballF'] = self.ballFormationList.get(first=None, last=None)
 
@@ -71,10 +81,11 @@ class simulation(tkinter.Tk):
         simArgs['initX'] = self.initialXScale.get()
         simArgs['initY'] = self.initialYScale.get()
         simArgs['playbackSpeed'] = self.playbackSpeedScale.get()
-        game=sim.PygameSim(**simArgs)
+        game = sim.PygameSim(**simArgs)
         game.main()
 
+
 if __name__ == '__main__':
-    app=simulation(None)
+    app = simulation(None)
     app.title('Simulation')
     app.mainloop()
