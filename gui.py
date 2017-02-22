b35c14e185b9942246bee8912c369816558eb59b
Ryan Schmidt <ryan.schmidt.604@gmail.com>
2017-02-21 20:41:00 -0800

    added image to gui and cleaned up code

--- gui.py
+++ gui.py
@@ -1,26 +1,45 @@
 import tkinter
-import pygame_test as game
+import simulation as sim
+from PIL import Image, ImageTk
+import platform
 
-class app_tk(tkinter.Tk):
+class simulation(tkinter.Tk):
     def __init__(self,parent):
         tkinter.Tk.__init__(self,parent)
         self.parent = parent
         self.initialize()
 
     def initialize(self):
-        self.grid()
+        self.grid() #sets up grid
 
-        button = tkinter.Button(self,text=u"Click me!", command=self.OnButtonClick)
-        button.grid(column=1,row=0)
+        #define a button to start simulation, runs startSimulation() when
+        #cliked
+        button = tkinter.Button(self,text=u'Start simulation',
+            command=self.startSimulation)
+        button.grid(column=1,row=1)
+        #open image using PIL
+        if platform.system == 'Windows':
+            image=Image.open('images\HLS-EFS-CSC-Owl.bmp')
+        else:
+            image=Image.open('images/HLS-EFS-CSC-Owl.bmp')
+        #make Tk compatible PhotoImage object, must save as object parameter
+        #to avoid garbage collection
+        self.photo=ImageTk.PhotoImage(image)
+        #make canvas and add photo
+        preview = tkinter.Canvas(self,width=275, height=183)
+        preview.grid(column=2,row=1)
+        preview.create_image(0,0, anchor='nw',image=self.photo)
+
+
 
         self.grid_columnconfigure(0,weight=1)
-        self.resizable(True,False)
+        #self.resizable(True,False)
 
-    def OnButtonClick(self):
-        game.main()
+    def startSimulation(self):
+        sim.main()
 
 
 if __name__ == '__main__':
-    app=app_tk(None)
+    app=simulation(None)
     app.title('Simulation')
     app.mainloop()
